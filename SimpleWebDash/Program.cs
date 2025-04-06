b1a15002f02adeb2362ae6f085c41dee1af483ce
Maciej Cichocki <maciek.ce3@gmail.com>
2025-04-06 11:14:00 +0200

    Add IpDetailsDataEndpoint and improve config handling
    - Introduced `IpDeteailsDataEndpoint` class for processing IP details requests, with a placeholder for backend logic.
    - Made minor adjustments to `index.html` for HTML structure.
    - Enhanced configuration handling in `Program.cs` with additional user input options and improved error handling. resolves #2

--- SimpleWebDash/Program.cs
+++ SimpleWebDash/Program.cs
@@ -6,9 +6,12 @@ using NetBase;
 using NetBase.Communication;
 using NetBase.FileProvider;
 using NetBase.StaticRouting;
+using Newtonsoft.Json.Linq;
 using SimpleWebDash.Endpoints;
 using SimpleWebDash.Monitors;
 using SimpleWebDash.Monitors.Data;
+using System.IO;
+using System;
 
 namespace SimpleWebDash
 {
@@ -17,18 +20,67 @@ namespace SimpleWebDash
 		private static List<DataEndpoint> endpoints;
 		static void Main(string[] args)
 		{
-			ReturnCode code = Menu.Show(new List<MenuItem>
+			// replace this mess with a config file if a config file is passed through the command line as an argument
+			// could be a json file or an ini file
+			string IPPath = null;
+			string HTTPPath = null;
+			string TEMPSPath = null;
+			string IPAddres = null;
+			int Port = 8080;
+			bool IsReadOnlyNode = true;
+			if (args.Length != 1)
 			{
-				new TextboxMenuItem("IP Monitor Data Path", "ips.c.json"),
-				new TextboxMenuItem("HTTP Monitor Data Path", "https.c.json"),
-				new TextboxMenuItem("TEMPS Monitor Data Path", "temps.c.json"),
-				new CheckboxMenuItem("ReadOnlyNode", true),
-				new MenuItem("Continue")
-			});
-			string IPPath = code.Textboxes[0];
-			string HTTPPath = code.Textboxes[1];
-			string TEMPSPath = code.Textboxes[2];
-			bool IsReadOnlyNode = code.Checkboxes[0];
+				ReturnCode code = Menu.Show(new List<MenuItem>
+				{
+					new TextboxMenuItem("IP Monitor Data Path", "ips.c.json"),
+					new TextboxMenuItem("HTTP Monitor Data Path", "https.c.json"),
+					new TextboxMenuItem("TEMPS Monitor Data Path", "temps.c.json"),
+					new CheckboxMenuItem("ReadOnlyNode", true),
+					new TextboxMenuItem("IP Address", "127.0.0.1"),
+					new NumboxMenuItem("Port", 8080),
+					new MenuItem("Continue")
+				});
+				IPPath = code.Textboxes[0];
+				HTTPPath = code.Textboxes[1];
+				TEMPSPath = code.Textboxes[2];
+				IPAddres = code.Textboxes[3];
+				Port = code.Numboxes[0];
+				IsReadOnlyNode = code.Checkboxes[0];
+			}
+			else
+			{
+				try
+				{
+					if (!File.Exists(args[0]))
+					{
+						Console.Error.WriteLine("File not found: " + args[0]);
+						Environment.Exit(1);
+					}
+					dynamic config = JObject.Parse(File.ReadAllText(args[0]));
+					IPPath = config["IPMonitorDataPath"].Value;
+					HTTPPath = config["HTTPMonitorDataPath"].Value;
+					TEMPSPath = config["TEMPSMonitorDataPath"].Value;
+					IPAddres = config["IPAddress"].Value;
+					Port = config["Port"].Value;
+					IsReadOnlyNode = config["ReadOnlyNode"].Value;
+				}
+				catch (Exception)
+				{
+					Console.Error.WriteLine("Error parsing config file");
+					Console.Error.WriteLine("Please make sure the config file is in the correct format");
+					Console.Error.WriteLine("The config file should be a json file with the following format:");
+					Console.Error.WriteLine("{");
+					Console.Error.WriteLine("  \"IPMonitorDataPath\": \"ips.c.json\",");
+					Console.Error.WriteLine("  \"HTTPMonitorDataPath\": \"https.c.json\",");
+					Console.Error.WriteLine("  \"TEMPSMonitorDataPath\": \"temps.c.json\",");
+					Console.Error.WriteLine("  \"IPAddress\": \"127.0.0.1\",");
+					Console.Error.WriteLine("  \"Port\": 8080,");
+					Console.Error.WriteLine("  \"ReadOnlyNode\": true");
+					Console.Error.WriteLine("}");
+					Environment.Exit(1);
+				}
+			}
+
 			Server.router = DataRecieved;
 			Monitor[] monitors = {
 				new IpMonitor("192.168.10.149"),
@@ -55,7 +107,7 @@ namespace SimpleWebDash
 			HttpMonitorDataManager.Initialize(HTTPPath);
 			TemperatureMonitorDataManager.Initialize(TEMPSPath);
 			if (!IsReadOnlyNode) { Clock.Start(); }
-			Server.Start(IPAddress.Loopback, 8080);
+			Server.Start(IPAddress.Parse(IPAddres), Port);
 			while (true) { }
 		}
 		public static HttpResponse DataRecieved(HttpRequest request)
